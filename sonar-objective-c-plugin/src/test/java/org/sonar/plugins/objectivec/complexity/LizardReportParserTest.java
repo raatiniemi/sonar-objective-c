7efa4b229bf3f68716b486cbb21a482b3e9a0b9d
Tobias Raatiniemi <raatiniemi@gmail.com>
2018-06-08 06:12:54 +0200

    update copyright headers for changed files

--- sonar-objective-c-plugin/src/test/java/org/sonar/plugins/objectivec/complexity/LizardReportParserTest.java
+++ sonar-objective-c-plugin/src/test/java/org/sonar/plugins/objectivec/complexity/LizardReportParserTest.java
@@ -1,6 +1,6 @@
-/**
- * backelite-sonar-objective-c-plugin - Enables analysis of Objective-C projects into SonarQube.
+/*
  * Copyright © 2012 OCTO Technology, Backelite (${email})
+ * Copyright (c) 2018 Tobias Raatiniemi
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,25 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.sonar.plugins.objectivec.complexity;/*
- * Sonar Objective-C Plugin
- * Copyright (C) 2012 OCTO Technology, Backelite
- * dev@sonar.codehaus.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
- */
+package org.sonar.plugins.objectivec.complexity;
 
 import org.junit.Before;
 import org.junit.Rule;
