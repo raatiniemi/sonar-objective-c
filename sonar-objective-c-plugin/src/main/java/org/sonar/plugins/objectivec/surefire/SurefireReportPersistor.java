088d0d8e75b9ed8987373914c072008db335cc6a
Tobias Raatiniemi <raatiniemi@gmail.com>
2018-07-02 20:41:03 +0200

    annotate arguments for get input file

--- sonar-objective-c-plugin/src/main/java/org/sonar/plugins/objectivec/surefire/SurefireReportPersistor.java
+++ sonar-objective-c-plugin/src/main/java/org/sonar/plugins/objectivec/surefire/SurefireReportPersistor.java
@@ -83,7 +83,7 @@ final class SurefireReportPersistor {
         return className + ".m";
     }
 
-    private void getInputFile(InputFile inputFile, TestSuite testSuite) {
+    private void getInputFile(@Nonnull InputFile inputFile, @Nonnull TestSuite testSuite) {
         saveMeasure(inputFile, CoreMetrics.TESTS, testSuite.getNumberOfTests());
         saveMeasure(inputFile, CoreMetrics.TEST_FAILURES, testSuite.getNumberOfFailedTests());
         saveMeasure(inputFile, CoreMetrics.TEST_EXECUTION_TIME, testSuite.getDurationInMilliseconds());
