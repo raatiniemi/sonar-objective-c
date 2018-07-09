050aaf7513f104ed56635856e54d93adbc2ed553
Tobias Raatiniemi <raatiniemi@gmail.com>
2018-07-09 11:26:43 +0200

    convert CoberturaSensor to use new Sensor interface

--- sonar-objective-c-plugin/src/main/java/org/sonar/plugins/objectivec/coverage/CoberturaSensor.java
+++ sonar-objective-c-plugin/src/main/java/org/sonar/plugins/objectivec/coverage/CoberturaSensor.java
@@ -19,43 +19,47 @@ package org.sonar.plugins.objectivec.coverage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.sonar.api.batch.Sensor;
 import org.sonar.api.batch.SensorContext;
-import org.sonar.api.batch.fs.FileSystem;
+import org.sonar.api.batch.sensor.Sensor;
+import org.sonar.api.batch.sensor.SensorDescriptor;
 import org.sonar.api.config.Settings;
-import org.sonar.api.resources.Project;
 import org.sonar.plugins.objectivec.ObjectiveCPlugin;
 import org.sonar.plugins.objectivec.core.ObjectiveC;
 
+import javax.annotation.Nonnull;
 import java.io.File;
 
 public final class CoberturaSensor implements Sensor {
     private static final Logger LOGGER = LoggerFactory.getLogger(CoberturaSensor.class);
 
+    private static final String NAME = "Cobertura sensor";
     public static final String REPORT_PATTERN_KEY = ObjectiveCPlugin.PROPERTY_PREFIX
             + ".coverage.reportPattern";
     public static final String DEFAULT_REPORT_PATTERN = "sonar-reports/coverage*.xml";
 
     private final ReportFilesFinder reportFilesFinder;
 
-    private final FileSystem fileSystem;
-
-    public CoberturaSensor(final FileSystem fileSystem, final Settings settings) {
-        this.fileSystem = fileSystem;
-
+    public CoberturaSensor(final Settings settings) {
         reportFilesFinder = new ReportFilesFinder(settings, REPORT_PATTERN_KEY, DEFAULT_REPORT_PATTERN);
     }
 
-    public boolean shouldExecuteOnProject(final Project project) {
-        return project.isRoot() && fileSystem.languages().contains(ObjectiveC.KEY);
-    }
-
-    public void analyse(final Project project, final SensorContext context) {
-        final String projectBaseDir = fileSystem.baseDir().getPath();
+    private void analyse(final SensorContext context) {
+        final String projectBaseDir = context.fileSystem().baseDir().getPath();
 
         for (final File report : reportFilesFinder.reportsIn(projectBaseDir)) {
             LOGGER.info("Processing coverage report {}", report);
-            CoberturaReportParser.parseReport(report, fileSystem, project, context);
+            CoberturaReportParser.parseReport(report, context.fileSystem(), null, context);
         }
+    }
+
+    @Override
+    public void describe(@Nonnull SensorDescriptor descriptor) {
+        descriptor.name(NAME);
+        descriptor.onlyOnLanguage(ObjectiveC.KEY);
+    }
+
+    @Override
+    public void execute(@Nonnull org.sonar.api.batch.sensor.SensorContext context) {
+        analyse((SensorContext) context);
     }
 }
