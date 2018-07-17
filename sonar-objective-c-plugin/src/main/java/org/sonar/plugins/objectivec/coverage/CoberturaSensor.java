ff003a994fccec328e105412c9b48617b7d71e83
Tobias Raatiniemi <raatiniemi@gmail.com>
2018-07-17 22:17:27 +0200

    replace parse cobertura report implementation

--- sonar-objective-c-plugin/src/main/java/org/sonar/plugins/objectivec/coverage/CoberturaSensor.java
+++ sonar-objective-c-plugin/src/main/java/org/sonar/plugins/objectivec/coverage/CoberturaSensor.java
@@ -19,38 +19,34 @@ package org.sonar.plugins.objectivec.coverage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.sonar.api.batch.SensorContext;
 import org.sonar.api.batch.sensor.Sensor;
+import org.sonar.api.batch.sensor.SensorContext;
 import org.sonar.api.batch.sensor.SensorDescriptor;
 import org.sonar.api.config.Settings;
 import org.sonar.plugins.objectivec.ObjectiveCPlugin;
 import org.sonar.plugins.objectivec.core.ObjectiveC;
 
 import javax.annotation.Nonnull;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class CoberturaSensor implements Sensor {
     private static final Logger LOGGER = LoggerFactory.getLogger(CoberturaSensor.class);
-
     private static final String NAME = "Cobertura sensor";
     public static final String REPORT_PATTERN_KEY = ObjectiveCPlugin.PROPERTY_PREFIX
             + ".coverage.reportPattern";
     public static final String DEFAULT_REPORT_PATTERN = "sonar-reports/coverage*.xml";
 
-    private final ReportFilesFinder reportFilesFinder;
+    private final Settings settings;
 
     @SuppressWarnings("WeakerAccess")
     public CoberturaSensor(final Settings settings) {
-        reportFilesFinder = new ReportFilesFinder(settings, REPORT_PATTERN_KEY, DEFAULT_REPORT_PATTERN);
-    }
-
-    private void analyse(final SensorContext context) {
-        final String projectBaseDir = context.fileSystem().baseDir().getPath();
-
-        for (final File report : reportFilesFinder.reportsIn(projectBaseDir)) {
-            LOGGER.info("Processing coverage report {}", report);
-            CoberturaReportParser.parseReport(report, context);
-        }
+        this.settings = settings;
     }
 
     @Override
@@ -60,7 +56,43 @@ public final class CoberturaSensor implements Sensor {
     }
 
     @Override
-    public void execute(@Nonnull org.sonar.api.batch.sensor.SensorContext context) {
-        analyse((SensorContext) context);
+    public void execute(@Nonnull SensorContext context) {
+        List<CoberturaPackage> availableReports = collectAndParseAvailableReports(context);
+
+        ReportPersistor reportPersistor = ReportPersistor.create(context);
+        reportPersistor.saveReports(availableReports);
+    }
+
+    @Nonnull
+    private List<CoberturaPackage> collectAndParseAvailableReports(@Nonnull SensorContext context) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            ReportParser reportParser = ReportParser.create(factory.newDocumentBuilder());
+
+            return collectAvailableReports(context)
+                    .map(reportParser::parse)
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList());
+        } catch (ParserConfigurationException e) {
+            LOGGER.error("Unable to create document builder", e);
+            return Collections.emptyList();
+        }
+    }
+
+    @Nonnull
+    private Stream<File> collectAvailableReports(@Nonnull SensorContext context) {
+        return ReportCollector.collect(context.fileSystem().baseDir().getAbsolutePath(), getReportFilePattern()).stream();
+    }
+
+    @Nonnull
+    private String getReportFilePattern() {
+        String reportPath = settings.getString(REPORT_PATTERN_KEY);
+
+        if (reportPath == null) {
+            LOGGER.debug("No value specified for \"" + REPORT_PATTERN_KEY + "\" using default report pattern");
+            reportPath = DEFAULT_REPORT_PATTERN;
+        }
+
+        return reportPath;
     }
 }
--- sonar-objective-c-plugin/src/test/java/org/sonar/plugins/objectivec/coverage/CoberturaSensorTest.java
+++ sonar-objective-c-plugin/src/test/java/org/sonar/plugins/objectivec/coverage/CoberturaSensorTest.java
@@ -17,25 +17,71 @@
 package org.sonar.plugins.objectivec.coverage;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.sonar.api.batch.fs.InputFile;
+import org.sonar.api.batch.fs.internal.DefaultInputFile;
 import org.sonar.api.batch.sensor.internal.DefaultSensorDescriptor;
+import org.sonar.api.batch.sensor.internal.SensorContextTester;
 import org.sonar.api.config.MapSettings;
 import org.sonar.plugins.objectivec.core.ObjectiveC;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class CoberturaSensorTest {
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    private final Path resourcePath = Paths.get("src", "test", "resources", "cobertura");
     private final MapSettings settings = new MapSettings();
 
+    private SensorContextTester context;
     private CoberturaSensor sensor;
+
+    private DefaultInputFile classNameFile;
 
     @Before
     public void setUp() {
+        context = SensorContextTester.create(temporaryFolder.getRoot());
         sensor = new CoberturaSensor(settings);
+
+        classNameFile = createFile();
+    }
+
+    @Nonnull
+    private DefaultInputFile createFile() {
+        return new DefaultInputFile(context.module().key(), "TargetName/ClassName.m")
+                .setLanguage("bla")
+                .setType(InputFile.Type.MAIN)
+                .initMetadata("1\n2\n3\n4\n5\n6");
+    }
+
+    private void addFileToFs(@Nonnull DefaultInputFile inputFile) {
+        context.fileSystem().add(inputFile);
+    }
+
+    private void createReportFile(@Nonnull String relativePath) {
+        try {
+            List<String> reportLines = Files.readAllLines(Paths.get(resourcePath.toString(), "cobertura.xml"));
+
+            Path destination = Paths.get(temporaryFolder.getRoot().getAbsolutePath(), relativePath);
+            Files.createDirectories(destination.getParent());
+            Files.createFile(destination);
+            Files.write(destination, reportLines);
+        } catch (IOException e) {
+            fail(String.format("Unable to create report file: %s", e.getMessage()));
+        }
     }
 
     @Test
@@ -46,5 +92,31 @@ public class CoberturaSensorTest {
 
         assertEquals("Cobertura sensor", descriptor.name());
         assertTrue(descriptor.languages().contains(ObjectiveC.KEY));
+    }
+
+    @Test
+    public void execute_withDefaultReportPattern() {
+        addFileToFs(classNameFile);
+        createReportFile("sonar-reports/coverage.xml");
+
+        sensor.execute(context);
+
+        assertEquals(Integer.valueOf(1), context.lineHits(classNameFile.key(), 1));
+    }
+
+    @Test
+    public void execute_withReportPattern() {
+        settings.setProperty("sonar.objectivec.coverage.reportPattern", "cobertura.xml");
+        addFileToFs(classNameFile);
+        createReportFile("cobertura.xml");
+
+        sensor.execute(context);
+
+        assertEquals(Integer.valueOf(1), context.lineHits(classNameFile.key(), 1));
+    }
+
+    @Test
+    public void execute_withoutReports() {
+        sensor.execute(context);
     }
 }
