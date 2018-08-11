8e58c0e78c08981098f6538583a32d9c637270b3
Tobias Raatiniemi <raatiniemi@gmail.com>
2018-08-11 08:54:08 +0200

    use TestInputFileBuilder to build DefaultInputFile
    when migrating the code base to gradle, the implementation of one of the
    dependencies seemed to have changed. `DefaultInputFile` can no longer be
    built by specifing the module key and a relative path, instead the
    `TestInputFileBuilder` have to be used.

--- plugin/src/test/java/me/raatiniemi/sonarqube/FileSystemHelpers.java
+++ plugin/src/test/java/me/raatiniemi/sonarqube/FileSystemHelpers.java
@@ -2,6 +2,7 @@ package me.raatiniemi.sonarqube;
 
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.fs.internal.DefaultInputFile;
+import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
 import org.sonar.api.batch.sensor.internal.SensorContextTester;
 
 import javax.annotation.Nonnull;
@@ -24,8 +25,8 @@ public final class FileSystemHelpers {
     }
 
     @Nonnull
-    private DefaultInputFile buildInputFile(@Nonnull String relativePath, @Nonnull String language) {
-        return new DefaultInputFile(context.module().key(), relativePath)
+    private TestInputFileBuilder buildInputFile(@Nonnull String relativePath, @Nonnull String language) {
+        return new TestInputFileBuilder(context.module().key(), relativePath)
                 .setLanguage(language)
                 .initMetadata("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n");
     }
@@ -33,13 +34,15 @@ public final class FileSystemHelpers {
     @Nonnull
     public DefaultInputFile createFile(@Nonnull String relativePath, @Nonnull String language) {
         return buildInputFile(relativePath, language)
-                .setType(InputFile.Type.MAIN);
+                .setType(InputFile.Type.MAIN)
+                .build();
     }
 
     @Nonnull
     public DefaultInputFile createTestFile(@Nonnull String relativePath, @Nonnull String language) {
         return buildInputFile(relativePath, language)
-                .setType(InputFile.Type.TEST);
+                .setType(InputFile.Type.TEST)
+                .build();
     }
 
     public void addToFileSystem(@Nonnull DefaultInputFile inputFile) {
