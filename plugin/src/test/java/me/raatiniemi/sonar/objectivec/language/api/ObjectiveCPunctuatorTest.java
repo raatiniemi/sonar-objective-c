db58002df452001a5619af39da8b88fad5272b96
Tobias Raatiniemi <raatiniemi@gmail.com>
2018-09-01 13:42:44 +0200

    move plugin module to new package

---
+++ plugin/src/test/java/me/raatiniemi/sonar/objectivec/language/api/ObjectiveCPunctuatorTest.java
@@ -0,0 +1,31 @@
+/**
+ * backelite-sonar-objective-c-plugin - Enables analysis of Objective-C projects into SonarQube.
+ * Copyright © 2012 OCTO Technology, Backelite (${email})
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.raatiniemi.sonar.objectivec.language.api;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ObjectiveCPunctuatorTest {
+
+    @Test
+    public void test() {
+        Assert.assertThat(ObjectiveCPunctuator.values().length, Matchers.is(48));
+    }
+
+}
